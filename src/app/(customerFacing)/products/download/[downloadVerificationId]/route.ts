c121b0e5b9da31fc3089e4f59ef7d44cb7d4f0a2
Srdjan Jevtic <srdjanjevticle@gmail.com>
2025-03-17 22:52:58 +0100

    download route

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -2,12 +2,15 @@ import prisma from "@/db/db"
 import { NextRequest, NextResponse } from "next/server"
 import fs from "fs/promises"
 
+type Params = Promise<{ downloadVerificationId: string }>
+
 export async function GET(
   req: NextRequest,
   {
-    params: { downloadVerificationId },
-  }: { params: { downloadVerificationId: string } }
+    params,
+  }: { params: Params }
 ) {
+  const { downloadVerificationId } = await params;
   const data = await prisma.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
