2605c80f8bef192e093dca25e33452519b50899f
Srdjan Jevtic <srdjanjevticle@gmail.com>
2025-03-17 22:31:58 +0100

    final

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -40,7 +40,7 @@ export function ProductCard({
       </CardContent>
       <CardFooter>
         <Button asChild size="lg" className="w-full">
-          <Link href={`/products/${id}/purchase`}>Purchase</Link>
+          <Link href={`/products/${id}`}>Purchase</Link>
         </Button>
       </CardFooter>
     </Card>
