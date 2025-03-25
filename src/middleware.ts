6a65392101df532ee83866d7b980aafac0dbeb06
Srdjan Jevtic <srdjanjevticle@gmail.com>
2025-03-25 14:29:08 +0100

    middleware patch

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 import { isValidPassword } from "./lib/isValidPassword"
 
 export async function middleware(req: NextRequest) {
