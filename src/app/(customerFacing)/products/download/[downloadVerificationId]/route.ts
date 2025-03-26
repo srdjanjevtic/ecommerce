f9c1266e110beeb53c2e4d2b687e5375d7518eb2
Srdjan Jevtic <srdjanjevticle@gmail.com>
2025-03-26 21:44:08 +0100

    route.ts

--- src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -1,8 +1,8 @@
-import prisma from "@/db/db";
-import { NextRequest, NextResponse } from "next/server";
-import { head } from '@vercel/blob';
+import prisma from "@/db/db"
+import { NextRequest, NextResponse } from "next/server"
+import { head } from '@vercel/blob'
 
-type Params = Promise<{ downloadVerificationId: string }>;
+type Params = Promise<{ downloadVerificationId: string }>
 
 export async function GET(
   req: NextRequest,
@@ -10,31 +10,32 @@ export async function GET(
     params,
   }: { params: Params }
 ) {
-  const { downloadVerificationId } = await params;
+  const { downloadVerificationId } = await params
   const data = await prisma.downloadVerification.findUnique({
     where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
     select: { product: { select: { filePath: true, name: true } } },
-  });
+  })
 
   if (data == null) {
-    return NextResponse.redirect(new URL("/products/download/expired", req.url));
+    return NextResponse.redirect(new URL("/products/download/expired", req.url))
   }
 
-  const metadata = await head(data.product.filePath);
-  console.log("metadata", metadata);
+  const metadata = await head(data.product.filePath)
+  // console.log("metadata", metadata)
   
-  const extension = data.product.filePath.split(".").pop();
-
-  // Fetch the blob from the download URL
-  const response = await fetch(metadata.downloadUrl);
-  const blob = await response.blob();
-
-  // Return the file for download
-  return NextResponse.json(blob, {
-    status: 200,
-    headers: {
-      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
-      "Content-Length": metadata.size.toString(),
-    },
-  });
+  // const extension = data.product.filePath.split(".").pop()
+  
+  // // Fetch the blob from the download URL
+  // const response = await fetch(metadata.downloadUrl)
+  // const blob = await response.blob()
+  
+  // // Return the file for download
+  return NextResponse.redirect(metadata.downloadUrl)
+  // return NextResponse.json(blob, {
+  //   status: 200,
+  //   headers: {
+  //     "Content-Disposition": `attachment filename="${data.product.name}.${extension}"`,
+  //     "Content-Length": metadata.size.toString(),
+  //   },
+  // })
 }
