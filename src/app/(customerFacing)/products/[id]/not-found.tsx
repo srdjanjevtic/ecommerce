5bc68931e01c7ccbb1b3bb7f04d9c901c8efe0dc
Srdjan Jevtic <srdjanjevticle@gmail.com>
2025-03-27 17:12:48 +0100

    not-found page completed

--- src/app/(customerFacing)/products/[id]/not-found.tsx
+++ src/app/(customerFacing)/products/[id]/not-found.tsx
@@ -1,7 +1,20 @@
-const notFoundPage = () => {
+"use client"
+
+import { Link } from "lucide-react"
+import { usePathname } from "next/navigation"
+
+const NotFoundPage = () => {
+    const pathName = usePathname()
+    const productId = pathName.split("/").pop()
+
   return (
-    <div className="text-center max-w-8xl mx-auto">Product not found</div>
+
+    <div className="text-center max-w-8xl mx-auto">
+        <span>{`Product ${productId} not found`}</span>
+        <Link href="/products" className="text-blue-500">Products</Link>
+        </div>
+
   )
 }
 
-export default notFoundPage
+export default NotFoundPage
